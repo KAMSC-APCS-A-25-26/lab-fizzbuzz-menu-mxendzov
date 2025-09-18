b8623ef1d25cd960e3a23ad668df361920935837
mxendzov <mxendzov17@gmail.com>
2025-09-18 13:47:16 -0400

    Update FizzBuzzMenu.java

--- src/main/java/FizzBuzzMenu.java
+++ src/main/java/FizzBuzzMenu.java
@@ -1,8 +1,49 @@
 import java.util.Scanner;
 
-// DO NOT CHANGE THE CLASS NAME, IT WILL BREAK THE AUTO GRADER
 public class FizzBuzzMenu {
     public static void main(String[] args) {
-        // TODO WRITE CODE HERE
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        boolean run = true;
+        while (run) {
+            System.out.println("--- FizzBuzz Menu ---");
+            System.out.println("1. Fizz (Multiples of 3)");
+            System.out.println("2. Buzz (Multiples of 5)");
+            System.out.println("3. FizzBuzz (Multiples of 3 and 5)");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    for (int i = 1; i <= 100; i++) {
+                        if (i % 3 == 0)
+                            System.out.print(i + " ");
+                        }
+                        System.out.println();
+                        break;
+
+                case 2:
+                    for (int i = 1; i <= 100; i++) {
+                        if (i % 5 == 0)
+                            System.out.print(i + " ");
+                    }
+                    System.out.println();
+                    break;
+                case 3:
+                    for (int i = 1; i <= 100; i++) {
+                        if (i % 15 == 0)
+                            System.out.print(i + " ");
+                    }
+                    System.out.println();
+                    break;
+
+                case 4:
+                    System.out.println("Goodbye!");
+                    run = false;
+                    System.out.println();
+                    break;
+
+            }
+        }
     }
 }
